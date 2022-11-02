262323fbe101bbbda2f41b87c9a1224a2f4a59c3
Abraham Huacchillo <huacchillocastillo@gmail.com>
2022-11-02 10:31:51 -0500

    add register and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 const App = () => {
-  return <Home />
+  return <Login />
 }
 
 export default App
